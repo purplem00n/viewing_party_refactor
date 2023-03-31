7b2d432e798ebbc8c541ab87f168be5f43aa2d3e
Ariel Higuera <purpl3m00ns@gmail.com>
2023-03-31 15:08:55 -0700

    initialized Movie and wrote 2 movie tests

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -1,9 +1,18 @@
 import pytest
+from viewing_party.movie import Movie
 
-def test_1():
+def test_movie_name_and_genre_and_rating_set_correctly():
     # Arrange
-
+    matrix = Movie("Matrix", "Sci-Fi", 3)
     # Act
 
     # Assert
-    pass
+    assert matrix.name == "Matrix"
+    assert matrix.genre == "Sci-Fi"
+    assert matrix.rating == 3
+
+def test_rating_between_1_and_5():
+    #Arrange
+    matrix = Movie("Matrix", "Sci-Fi", 3)
+
+    assert matrix.rating in [1,2,3,4,5]
